c971baae96bcfd0a1884a66fac2fe705ff549fd4
amiraabdelazez <amiraabdelazez2@gmail.com>
2024-12-17 00:10:20 +0200

    String Formatting

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
+            #region String Formatting
+            //int X=3,Y=4;
+            //string Message = "Equation : " + X + "+" + Y + "=" + (X + Y);
+            //Console.WriteLine(Message);
+
+            //string Message2 = string.Format("Equation : {0}+{1}={2}", X, Y, X + Y);
+            //Console.WriteLine(Message2);
+
+            //string message3 = $"Equation : {X}+{Y}={X + Y}";
+            //Console.WriteLine(message3);  
+            #endregion
         }
     }
 }
